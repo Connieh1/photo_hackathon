5894db0c59307b53ac0a6109d5995c571c1ade9e
github username <constance.hyman@mchsi.com>
2020-07-06 11:54:03 -0700

    Building fetch

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const endPoint = "https://unsplash.com"
+const endPoint = "https://api.pexels.com/v1/search?query=people"
+
 
 document.addEventListener('DOMContentLoaded', () =>{
   console.log("here!")
+  getPhotos();
+
+
   })
+
+
+function getPhotos(){
+  fetch(endPoint)
+  .then(response => console.log(response))
+ 
+}
